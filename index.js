f854aa1c638316dfd6b8ef085811e013eb2a63ef
michalak111 <michalak111@gmail.com>
2018-07-25 21:26:13 +0200

    generate react component

---
+++ index.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const argv = require('minimist')(process.argv.slice(2));
+const pjson = require('./package.json')
+
+const config = {
+  filesSourceDir: pjson.rctgen ? pjson.rctgen.sourceDir :'./src'
+}
+
+const camelCaseIntoLisp = string => string.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+
+const generateComponent = (COMPONENT_NAME) => {
+
+  const templateJS = `import * as React from 'react'
+
+export default ${COMPONENT_NAME}Component extends React.Component {
+  render () {
+    return (
+      <div>${COMPONENT_NAME}Component</div>
+    )
+  }
+}`
+
+const templateCSS = `.${camelCaseIntoLisp(COMPONENT_NAME)} {}`
+
+  if (!fs.existsSync(config.filesSourceDir)){
+    console.error('Source folder does not exist!')
+    return
+  }
+
+  fs.mkdir(`${config.filesSourceDir}/${COMPONENT_NAME}Component`, (err) => {
+    if (err) throw err;
+    console.log(`${COMPONENT_NAME}Component directory has been generated`)
+    fs.writeFile(`${config.filesSourceDir}/${COMPONENT_NAME}Component/${COMPONENT_NAME}Component.js`, templateJS, (err) => {
+      if (err) throw err;
+      console.log(`${COMPONENT_NAME}Component.js has been generated`);
+    });
+    fs.writeFile(`${config.filesSourceDir}/${COMPONENT_NAME}Component/${COMPONENT_NAME}Component.scss`, templateCSS, (err) => {
+      if (err) throw err;
+      console.log(`${COMPONENT_NAME}Component.scss has been generated`);
+    });
+  });
+}
+
+if (argv.hasOwnProperty('c')) {
+  if (typeof argv.c !== 'string') {
+    throw 'Component name is missing'
+  } else {
+    generateComponent(argv.c)
+  }
+}
