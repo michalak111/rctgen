663fa17eebb0279eb4feae4066a135980018c7e5
michalak111 <michalak111@gmail.com>
2018-07-30 14:48:24 +0200

    Update doc

---
+++ src/ButtonComponent/ButtonComponent.js
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import './ButtonComponent.scss'
+
+export default ButtonComponent extends React.Component {
+  render () {
+    return (
+      <div>ButtonComponent</div>
+    )
+  }
+}
